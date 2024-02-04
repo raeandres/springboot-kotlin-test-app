cdca2a3b060f4e9fcf567c036d650cfdc1cba369
Rae-An Andres <raeandres@users.noreply.github.com>
2024-02-04 20:15:30 +0800

    init commit

---
+++ src/main/kotlin/com/example/kotlinspring/kotlinspringtestapp/KotlinspringtestappApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspring.kotlinspringtestapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinspringtestappApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinspringtestappApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspring/kotlinspringtestapp/KotlinspringtestappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspring.kotlinspringtestapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinspringtestappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
